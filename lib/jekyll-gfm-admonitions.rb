wushenrong/jekyll-gfm-admonitions

commit 0cba248e5cbb57d649e2dca893af5c7c8900400e

v1.1.0 - custom titles

lib/jekyll-gfm-admonitions.rb
@@ -90,10 +90,14 @@ module JekyllGFMAdmonitions
     end
 
     def convert_admonitions(doc)
-      doc.content.gsub!(/>\s*\[!(IMPORTANT|NOTE|WARNING|TIP|CAUTION)\]\s*\n((?:>.*\n?)*)/) do
+      doc.content.gsub!(/>\s*\[!(IMPORTANT|NOTE|WARNING|TIP|CAUTION)\]\s*(.*)\s*\n((?:>.*\n?)*)/) do
         type = ::Regexp.last_match(1).downcase
-        title = type.capitalize
-        text = ::Regexp.last_match(2).gsub(/^>\s*/, '').strip
+        if ::Regexp.last_match(2).length > 0
+          title = ::Regexp.last_match(2)
+        else
+          title = type.capitalize
+        end
+        text = ::Regexp.last_match(3).gsub(/^>\s*/, '').strip
         icon = Octicons::Octicon.new(ADMONITION_ICONS[type]).to_svg
         Jekyll.logger.debug 'GFMA:', "Converting #{type} admonition."
 

jekyll-gfm-admonitions.gemspec
@@ -4,7 +4,7 @@
 
 Gem::Specification.new do |spec|
   spec.name          = 'jekyll-gfm-admonitions'
-  spec.version       = '1.0.4'
+  spec.version       = '1.1.0'
   spec.authors       = ['Robin De Schepper']
   spec.email         = ['robin.deschepper93@gmail.com']
 

sample-github-pages/index.markdown
@@ -7,9 +7,14 @@ layout: home
 
 # GitHub Flavored Admonitions
 
+Use GitHub Flavored Admonitions on your GitHub Pages or Jekyll site. Admonition titles are supported.
 
 > [!NOTE]
 > I am a **note** admonition, useful to let interested users take note of something.
+
+
+> [!TIP] Titles are supported
+> You can add custom titles to your admonitions.
 
 
 > [!TIP]

sample/index.markdown
@@ -7,9 +7,14 @@ layout: home
 
 # GitHub Flavored Admonitions
 
+Use GitHub Flavored Admonitions on your GitHub Pages or Jekyll site. Admonition titles are supported.
 
 > [!NOTE]
 > I am a **note** admonition, useful to let interested users take note of something.
+
+
+> [!TIP] Titles are supported
+> You can add custom titles to your admonitions.
 
 
 > [!TIP]

commit 91ed2ca18062d7dafad180ca7059143f72c654ce

Fix frozen string error. Aligned more with GitHub rendering (#8)
* Dupe the content before modifying.

This fixes an issue where we get: Error:  can't modify frozen String: ""

* Return early if the doc content is empty

Empty strings are frozen by default

* Ignore the `.DS_Store` folder

* Handle empty titles

0cba248e5cbb57d649e2dca893af5c7c8900400e introduced a bug when rendering titles.
First, it doesn't handle empty titles well. Second, the regex was too aggressive. It needs to stop at the newline.

This fixes that.

* The blockquote character should be at beginning of string

* Fix newlines in the block quote

* Handle indentation of block quotes

* Handle markdown after admonitions

The admonitions regex was capturing newlines after it. We need to give that one back.

* Don't add newlines

Follow the GitHub .md behavior, not the behavior in GitHub issues/pr.

.gitignore
@@ -3,3 +3,4 @@
 *.gem
 _site/
 Gemfile.lock
+.DS_Store

lib/jekyll-gfm-admonitions.rb
@@ -73,6 +73,12 @@ module JekyllGFMAdmonitions
     end
 
     def process_doc(doc)
+      # Return early if content is empty
+      return if doc.content.empty?
+
+      # If the content is frozen, we need to duplicate it so that we can modify it
+      doc.content = doc.content.dup unless doc.content.frozen?
+
       code_blocks = []
       # Temporarily replace code blocks by a tag, so that we don't process any admonitions
       # inside of code blocks.
@@ -90,26 +96,25 @@ module JekyllGFMAdmonitions
     end
 
     def convert_admonitions(doc)
-      doc.content.gsub!(/>\s*\[!(IMPORTANT|NOTE|WARNING|TIP|CAUTION)\]\s*(.*)\s*\n((?:>.*\n?)*)/) do
-        type = ::Regexp.last_match(1).downcase
-        if ::Regexp.last_match(2).length > 0
-          title = ::Regexp.last_match(2)
-        else
-          title = type.capitalize
-        end
-        text = ::Regexp.last_match(3).gsub(/^>\s*/, '').strip
-        icon = Octicons::Octicon.new(ADMONITION_ICONS[type]).to_svg
-        Jekyll.logger.debug 'GFMA:', "Converting #{type} admonition."
+      doc.content.gsub!(/^(\s*)>\s*\[!(IMPORTANT|NOTE|WARNING|TIP|CAUTION)\]([^\n]*)\n((?:\1\s*>\s*[^\n]*(?:\n|$))(?:(?!\s*>\s*\[!)\1\s*>\s*[^\n]*(?:\n|$))*)/) do
+        initial_indent = ::Regexp.last_match(1)
+        type = ::Regexp.last_match(2).downcase
+        title = ::Regexp.last_match(3).strip.empty? ? type.capitalize : ::Regexp.last_match(3).strip
+        text = ::Regexp.last_match(4).gsub(/^#{Regexp.escape(initial_indent)}\s*>\s*/, '').strip
 
+        icon = Octicons::Octicon.new(ADMONITION_ICONS[type]).to_svg
         admonition_html(type, title, text, icon)
       end
+
+      # 🛠 Ensure a blank line exists after each admonition block to prevent Markdown parsing issues.
+      doc.content.gsub!(/(<\/div>)(?!\n\n)/, "\\1\n\n")
     end
 
     def admonition_html(type, title, text, icon)
-      "<div class='markdown-alert markdown-alert-#{type}'>
-          <p class='markdown-alert-title'>#{icon} #{title}</p>
-          <p>#{@markdown.convert(text)}</p>
-        </div>\n\n"
+      "<div class='markdown-alert markdown-alert-#{type}'>" \
+        "<p class='markdown-alert-title'>#{icon} #{title}</p>" \
+        "#{@markdown.convert(text)}" \
+      "</div>"
     end
   end
 